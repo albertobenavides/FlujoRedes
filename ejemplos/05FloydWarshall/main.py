e945350e67b198788bb21c39dd1e3c7d14009d48
Alberto Benavides <bena.87@hotmail.com>
2018-04-07 18:58:17 -0500

    Ejemplos terminados.

--- ejemplos/05FloydWarshall/main.py
+++ ejemplos/05FloydWarshall/main.py
@@ -1,12 +1,11 @@
 # Documentación de modificaciones a los algoritmos de la profe
-# Revisar si funciona con grafos dirigidos
-# Crear grafos de diferentes tamaños y correr los algoritmos para graficar los tiempos que les toma a los algoritmos correrse; graficar con diagramas caja bigote y violín; y error bars (x, y, largo) y candlesticks (parte baja, alta, cuartil, cuartil, meidana)
+# Crear grafos de diferentes tamaños y correr los algoritmos para graficar los tiempos que les toma a los algoritmos correrse; graficar con diagramas caja bigote y violín; y error bars (x, y, largo) y candlesticks (parte baja, alta, cuartil, cuartil, mediana)
+# Aristas directamente proporcionales al número de nodos (por densidad, como un 0.1 de aristas por cantidad de nodos)
 # Revisar con pruebas estadísticas si los datos son normalmente distribuidos
 # ¡¡¡Ajuste de curvas!!!; gnuplot con fit()
 # Ver qué tanto se ajusta lo teórico esperado con lo experimental y describir los comportamientos
 # Definir curva teórica y experimental
 # Buscar en Folk-Fulkerson
-# Aristas directamente proporcionales al número de nodos (por densidad, como un 0.1 de aristas por cantidad de nodos)
 
 import sys
 sys.path.append('../../fuente/')
